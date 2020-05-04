2dcd4db15793e6eeb53d4b61566a03b9cdee2499
yangping2019102953 <55010732+yangping2019102953@users.noreply.github.com>
2020-05-04 11:40:25 +0800

    代码
    代码

---
+++ miniprogram-3 - 副本/pages/Main/logs.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    match_str:'',
+    regexp:'',
+    text3:'',
+
+  },
+ //input双向绑定
+ check1(e) {
+  this.setData({match_str: e.detail.value})
+},
+check2(e) {
+  this.setData({regexp: e.detail.value})
+},
+  primary1:function(options){
+    var str = this.data.match_str;
+    var reg=new RegExp(this.data.regexp,'g');
+     console.log(this.data.match_str);
+     console.log(this.data.regexp)
+     console.log(str.match(reg));
+    this.setData({
+     text3: str.match(reg)
+     })
+  }
+
+
+
+  
+})
